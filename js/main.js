852b9ffa0f11d2ee10cc551559847bdbf5f5d205
antonborod1n <antonborodin86@gmail.com>
2022-11-21 21:12:15 +0300

    added adaptive index

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function () {
     $('.header__languages-select').styler();
 })
+
+
+const filter = document.querySelector('.filter');
+const filterBtn = document.querySelector('.filter-btn');
+const filterClose = document.querySelector('.filter-btn-close');
+
+filterBtn.addEventListener('click', function () {
+    filter.classList.add('filter--active');
+});
+
+filterClose.addEventListener('click', function () {
+    filter.classList.remove('filter--active');
+});
