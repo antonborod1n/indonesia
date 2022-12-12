c60def440fe5d65dd57dfac7f342e8efa95c6742
antonborod1n <antonborodin86@gmail.com>
2022-12-12 11:09:08 +0300

    fix edits

--- public/js/main.js
+++ public/js/main.js
@@ -49,49 +49,54 @@ window.addEventListener('DOMContentLoaded', () => {
   //Render offers
   const mainOfferBox = document.querySelector('.offer__preview');
 
+
+
   function renderOffers(data) {
     const arr = [];
-    for (let i = 0; i < data.length; i++) {
-      arr.push(`
-    <div class="offer__item ${data[i].company.toLowerCase()} ${data[i].profile.toLowerCase()}">
-    <div class="offer__preview-top">
-      <div class="offer__preview-top-box">
-        <p class="offer__preview-top-title">${data[i].title}</p>
-        <div class="offer__preview-top-price">
-          <span>$</span>
-          <span>${data[i].priceFrom}</span>
-          <span>-</span>
-          <span>${data[i].priceTo}</span>
-        </div>
-      </div>
-      <div class="offer__preview-top-box">
-        <a class="offer__preview-top-link" href="#">${data[i].location}</a>
-        <a class="offer__preview-top-logo" href="#">
-          <img class="offer__preview-top-img" src="${data[i].imgLogo}" alt="Logo">
-        </a>
-      </div>
-    </div>
-    <p class="offer__lead">
-    ${data[i].text}
-    <a class="offer__lead-more" href="#">
-    more
-    <span class="material-symbols-outlined">
-      arrow_right_alt
-    </span>
-  </a>
-    </p>
-    <div class="offer__meta">
-    <a class="offer__meta-btn" href="#">
-    Apply
-    <span class="material-symbols-outlined">
-      arrow_right_alt
-    </span>
-  </a>
-      <span class="offer__meta-data">${data[i].date}</span>
-    </div>
-  </div>
-  `
-      )
+    if (data.length === 0) {
+      return mainOfferBox.innerHTML = "No vacancies found. Please try another description";
+    } else {
+      for (let i = 0; i < data.length; i++) {
+        arr.push(` <div class="offer__item ${data[i].company.toLowerCase()}">
+                  <div class="offer__preview-top">
+                    <div class="offer__preview-top-box">
+                      <p class="offer__preview-top-title">${data[i].title}</p>
+                      <div class="offer__preview-top-price-box">
+                        <div class="offer__preview-top-price">
+                          <span>$</span>
+                          <span>12100</span>
+                          <span>-</span>
+                          <span>12100</span>
+                        </div>
+                        <a class="offer__preview-top-logo" href="#">
+                          <img class="offer__preview-top-img" src="${data[i].imgLogo}" alt="Logo">
+                        </a>
+                      </div>
+                    </div>
+                    <a class="offer__preview-top-link" href="#">${data[i].location}</a>
+                  </div>
+                  <p class="offer__lead">
+                  ${data[i].text}
+                    <a class="offer__lead-more" href="#">
+                      more
+                      <span class="material-symbols-outlined">
+                        arrow_right_alt
+                      </span>
+                    </a>
+                  </p>
+                  <div class="offer__meta">
+                    <a class="offer__meta-btn" href="#">
+                      Apply
+                      <span class="material-symbols-outlined">
+                        arrow_right_alt
+                      </span>
+                    </a>
+                    <span class="offer__meta-data">${data[i].date}</span>
+                  </div>
+                </div>
+    `
+        )
+      }
     }
     return arr;
   }
@@ -147,6 +152,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const filterOffers = renderOffers(filterArr)
     mainOfferBox.innerHTML = filterOffers.join('');
   }
+
+
+
 })
 
 
